3157a95e9d41e0ddc3bf5306cc0e53a96da260e2
Mateus <99037253+mateuspdasilva@users.noreply.github.com>
2025-08-23 01:10:38 -0300

    feat: Add metrics retrieval route to the main application

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 
 from app.routes.get.experiments import router as get_experiments_router
+from app.routes.get.metrics import router as get_metrics_router
 from app.routes.post.ingest import router as ingest_experiments_router
 from app.routes.get.allocations import router as get_allocations_router
 
@@ -9,6 +10,7 @@ app = FastAPI()
 # Experiment routes
 app.include_router(get_experiments_router)
 app.include_router(ingest_experiments_router)
+app.include_router(get_metrics_router)
 
 # Allocation routes
 app.include_router(get_allocations_router)
